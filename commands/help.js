88b8dd26174c7e9b2cf95c34a46ad5fbda3eb817
paulleong97 <40818784+paulleong97@users.noreply.github.com>
2018-10-18 04:02:05 +0800

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -2,30 +2,12 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   let bicon = bot.user.displayAvatarURL;
-
-  let reason = args.join(" ").slice(22);
-
-  if(!reason){
-    // let helpEmbed = new Discord.RichEmbed()
-    // .setDescription("Commands")
-    // .setColor("#15f153")
-    // .setThumbnail(bicon)
-    // .addField("Initiate pk", "`;pk @name (;pk @rem)`")
-    // .addField("Use Skill", "``;l <skillname> (;l fr)``")
-    // .addField("Win/Loss/Elo record(Beta)", "```;pkrecord```")
-    // .addField("Leaderboard(Beta)", ";leaderboard")
-    //  message.channel.send(helpEmbed);
     let helpEmbed = new Discord.RichEmbed()
     .setDescription("Commands")
     .setColor("#15f153")
-    .setThumbnail(bicon)
-    .addField("Battle", "`pk` `l` `pkrecord` `leaderboard`")
-    .addField("meme", "`badnames` `wallofshame` `ayy` `single?`")
-    .addField("Moderation", "`purge` `addrole` `removerole`")
-    .addField("Information", " `serverinfo` `botinfo`")
+    .addField("Battle", "`turnbased` `spampk` `spaml` `pkrecord` `leaderboard`")
+    .addField("Moderation", "`prune`")
      message.channel.send(helpEmbed);
- }
-
 }
 
   module.exports.help = {
